2d6ed947170097b89b4d2cdd7cc582502eda740c
Vym38224 <jardaavymetal@gmail.com>
2026-03-16 10:21:32 +0100

    enable dragging for DES tables

--- app/src/components/cipher/des/components/DesPermutationModal.vue
+++ app/src/components/cipher/des/components/DesPermutationModal.vue
@@ -3,15 +3,18 @@
   <div 
     v-if="jeViditelny"
     class="ip-image-container"
-    :class="modalClass"
-    @click="zavrit"
+    :class="[modalClass, { 'is-dragging': isDragging }]"
+    :style="containerStyle"
+    @click.self="zavrit"
   >
-    <div class="ip-image-wrapper">
+    <div class="ip-scrollable">
+      <div class="ip-image-wrapper ip-draggable-handle" @mousedown="startDrag">
       <button class="ip-close-button" @click="zavrit">×</button>
       <img
         :src="cestaKObrazku"
         class="ip-image"
       />
+      </div>
     </div>
   </div>
 </template>
@@ -31,16 +34,29 @@ export default {
   },
   data() {
     return {
-      jeViditelny: false
+      jeViditelny: false,
+      isDragging: false,
+      dragStartX: 0,
+      dragStartY: 0,
+      offsetX: 0,
+      offsetY: 0
     };
   },
   computed: {
     cestaKObrazku() {
       return require(`@/assets/images/des/${this.tableType}.png`);
     },
+    containerStyle() {
+      return {
+        '--drag-x': `${this.offsetX}px`,
+        '--drag-y': `${this.offsetY}px`
+      };
+    }
   },
   methods: {
     otevrit() {
+      this.offsetX = 0;
+      this.offsetY = 0;
       this.jeViditelny = true;
     },
     zavrit() {
@@ -48,7 +64,41 @@ export default {
     },
     prepnout() {
       this.jeViditelny = !this.jeViditelny;
+    },
+    startDrag(event) {
+      if (event.button !== 0 || event.target.closest('.ip-close-button')) {
+        return;
+      }
+
+      this.isDragging = true;
+      this.dragStartX = event.clientX;
+      this.dragStartY = event.clientY;
+      event.preventDefault();
+    },
+    onDrag(event) {
+      if (!this.isDragging) {
+        return;
+      }
+
+      const deltaX = event.clientX - this.dragStartX;
+      const deltaY = event.clientY - this.dragStartY;
+
+      this.offsetX += deltaX;
+      this.offsetY += deltaY;
+      this.dragStartX = event.clientX;
+      this.dragStartY = event.clientY;
+    },
+    stopDrag() {
+      this.isDragging = false;
     }
+  },
+  mounted() {
+    window.addEventListener('mousemove', this.onDrag);
+    window.addEventListener('mouseup', this.stopDrag);
+  },
+  beforeUnmount() {
+    window.removeEventListener('mousemove', this.onDrag);
+    window.removeEventListener('mouseup', this.stopDrag);
   }
 };
 </script>
--- app/src/components/cipher/des/components/DesSboxModal.vue
+++ app/src/components/cipher/des/components/DesSboxModal.vue
@@ -3,12 +3,16 @@
   <div
     v-if="sboxNumber !== null"
     class="ip-image-container sbox"
+    :class="{ 'is-dragging': isDragging }"
+    :style="containerStyle"
     @click.self="zavrit"
   >
-    <div class="sbox-modal-wrapper">
+    <div class="sbox-modal-wrapper ip-draggable-handle" @mousedown="startDrag">
       <button class="sbox ip-close-button" @click="zavrit">×</button>
       
-      <div class="sbox-modal-content">
+      <div
+        class="sbox-modal-content"
+      >
         <h3>S-box {{ sboxNumber }}</h3>
 
         <legend class="sbox-legend flex">
@@ -16,41 +20,43 @@
           <span>sloupec: prostřední čtyři bity vstupu</span>
         </legend>
         
-        <table class="sbox-table">
-          <thead>
-            <tr>
-              <th class="corner-cell"></th>
-              <th 
-                v-for="col in 16" 
-                :key="'col-' + col" 
-                class="col-header"
-                :class="{ 'selected-cell': jeSloupecZvyraznen(col - 1) }"
-              >
-                {{ (col - 1).toString(2).padStart(4, '0') }}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(row, rowIndex) in ziskatDataSBoxu()" :key="'row-' + rowIndex">
-              <th 
-                class="row-header"
-                :class="{ 'selected-cell': jeRadekZvyraznen(rowIndex) }"
-              >
-                {{ rowIndex.toString(2).padStart(2, '0') }}
-              </th>
-              <td 
-                v-for="(value, colIndex) in row" 
-                :key="'cell-' + colIndex"
-                :class="{ 'selected-cell bold': jeBunkaVybrana(rowIndex, colIndex) }"
-              >
-                <div class="cell-content">
-                  <span class="decimal-value">{{ value }}</span>
-                  <span class="binary-value">{{ value.toString(2).padStart(4, '0') }}</span>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <div class="sbox-table-scroll">
+          <table class="sbox-table">
+            <thead>
+              <tr>
+                <th class="corner-cell"></th>
+                <th 
+                  v-for="col in 16" 
+                  :key="'col-' + col" 
+                  class="col-header"
+                  :class="{ 'selected-cell': jeSloupecZvyraznen(col - 1) }"
+                >
+                  {{ (col - 1).toString(2).padStart(4, '0') }}
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(row, rowIndex) in ziskatDataSBoxu()" :key="'row-' + rowIndex">
+                <th 
+                  class="row-header"
+                  :class="{ 'selected-cell': jeRadekZvyraznen(rowIndex) }"
+                >
+                  {{ rowIndex.toString(2).padStart(2, '0') }}
+                </th>
+                <td 
+                  v-for="(value, colIndex) in row" 
+                  :key="'cell-' + colIndex"
+                  :class="{ 'selected-cell bold': jeBunkaVybrana(rowIndex, colIndex) }"
+                >
+                  <div class="cell-content">
+                    <span class="decimal-value">{{ value }}</span>
+                    <span class="binary-value">{{ value.toString(2).padStart(4, '0') }}</span>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </div>
@@ -66,6 +72,11 @@ export default {
     return {
       sboxNumber: null, // číslo aktuálně zobrazeného S-boxu
       inputBits: null, // 6-bitový vstup pro S-box
+      isDragging: false,
+      dragStartX: 0,
+      dragStartY: 0,
+      offsetX: 0,
+      offsetY: 0,
     };
   },
   computed: {
@@ -77,11 +88,19 @@ export default {
       if (!this.inputBits) return null;
       return parseInt(this.inputBits.slice(1, 5), 2);
     },
+    containerStyle() {
+      return {
+        '--drag-x': `${this.offsetX}px`,
+        '--drag-y': `${this.offsetY}px`
+      };
+    }
   },
   methods: {
     otevrit(number, bits = null) {
       this.sboxNumber = number;
       this.inputBits = bits;
+      this.offsetX = 0;
+      this.offsetY = 0;
     },
     zavrit() {
       this.sboxNumber = null;
@@ -99,6 +118,40 @@ export default {
     jeBunkaVybrana(rowIndex, colIndex) {
       return this.vybranRadek === rowIndex && this.vybranSloupec === colIndex;
     },
+    startDrag(event) {
+      if (event.button !== 0 || event.target.closest('.ip-close-button')) {
+        return;
+      }
+
+      this.isDragging = true;
+      this.dragStartX = event.clientX;
+      this.dragStartY = event.clientY;
+      event.preventDefault();
+    },
+    onDrag(event) {
+      if (!this.isDragging) {
+        return;
+      }
+
+      const deltaX = event.clientX - this.dragStartX;
+      const deltaY = event.clientY - this.dragStartY;
+
+      this.offsetX += deltaX;
+      this.offsetY += deltaY;
+      this.dragStartX = event.clientX;
+      this.dragStartY = event.clientY;
+    },
+    stopDrag() {
+      this.isDragging = false;
+    },
+  },
+  mounted() {
+    window.addEventListener('mousemove', this.onDrag);
+    window.addEventListener('mouseup', this.stopDrag);
+  },
+  beforeUnmount() {
+    window.removeEventListener('mousemove', this.onDrag);
+    window.removeEventListener('mouseup', this.stopDrag);
   },
 };
 </script>
