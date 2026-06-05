d933e6885a4796ba29f70704ffddd93a8b3943a3
Jaroslav Vymětal <93612235+Vym38224@users.noreply.github.com>
2026-06-05 11:59:03 +0200

    new build with .htaccess

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/sifrovac/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/~vymeja02/sifrovac/" : "/",
   outputDir: "../docs",
 });
