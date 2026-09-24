1d8121d807f78697f98e5403ed9543efde9efe13
Jaroslav <jardaavymetal@gmail.com>
2026-09-24 13:05:09 +0200

    upozorňující komentář ohledně reálného šifrování u RSA

--- app/src/components/cipher/rsa/RsaCipherView.vue
+++ app/src/components/cipher/rsa/RsaCipherView.vue
@@ -329,6 +329,10 @@
               kroků.
             </p>
             <p>p × q musí být větší než 90</p>
+            <p class="warning-color"
+            style = "padding-left:33px; padding-right:33px;">
+              Upozornění: Pro demonstrační účely se převádí každý znak na ASCII a šifruje se samostatně. Běžně RSA šifruje číselné bloky zprávy.
+            </p>
           </div>
 
         </section>
